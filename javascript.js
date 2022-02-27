7aa5f690ab55d95ce8a48af9e442a4d0741dcaeb
Juuso Vuori <joosov@gmail.com>
2022-02-27 22:21:58 +0200

    0.5
    Added more images to references and gallery
    cleaned up the javascript code a bit
    removed the contact form to make proper validation later
    near-final design

---
+++ javascript.js
@@ -0,0 +1,24 @@
+ // Get the modal
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("work");
+
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+
+  // and attach our click listener for this image.
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
